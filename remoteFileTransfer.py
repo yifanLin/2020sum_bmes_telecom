778e1246d3372ee6a323c23e226cad16901866cf
yifanLin <51112522+yifanLin@users.noreply.github.com>
2020-08-01 17:22:34 -0700

    Create remoteFileTransfer.py

---
+++ remoteFileTransfer.py
@@ -0,0 +1,47 @@
+import tqdm
+import os
+
+SERVER_HOST = "192.168.1.82"
+SERVER_PORT = 5050
+# receive 4096 bytes each time
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen()
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+# accept connection if there is any
+client_socket, address = s.accept() 
+# if below code is executed, that means the sender is connected
+print(f"[+] {address} is connected.")
+
+# receive the file infos
+# receive using client socket, not server socket
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+# remove absolute path if there is
+filename = os.path.basename(filename)
+# convert to integer
+filesize = int(filesize)
+
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "wb") as f:
+    for _ in progress:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:    
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+
+# close the client socket
+client_socket.close()
+# close the server socket
+s.close()
