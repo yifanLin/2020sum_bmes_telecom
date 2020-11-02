319f5b46e56e0300a1cc7f40cfec64f9d3015259
yifanLin <51112522+yifanLin@users.noreply.github.com>
2020-11-01 18:22:56 -0800

    Create client_v1.py

---
+++ Fall2020/client_v1.py
@@ -0,0 +1,12 @@
+# File header: The following script receives a very simple message from this server to whatever client it connects to
+# Contributor: Jay Golden, Aayush Somani
+# latest update: 11/01/2020
+
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client connects to a certain server ip and port
+s.connect(('SERVER_IP', 5050))
+# ask the client to receive message, with a buffer size of     # 1024
+msg=s.recv(1024)
+# decode the msg with what it was originally encoded, which    # is utf-8 in this case
+print(msg.decode("utf-8"))
