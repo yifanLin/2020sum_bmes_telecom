77c1947b0e95cb33b61124697b223061f9c325ab
JayGolden55 <68171677+JayGolden55@users.noreply.github.com>
2021-01-31 16:30:33 -0800

    Create recvTrackData.py

---
+++ recvTrackData.py
@@ -0,0 +1,22 @@
+import pickle
+from socket import *
+
+# Socket Create
+socket = socket(AF_INET, SOCK_STREAM)
+
+host_ip = gethostbyname(gethostname())
+#host_ip = "192.168.0.1"
+Aayush_ip = "76.95.185.15"
+print('HOST IP:', host_ip)
+port = 40005
+socket_address = (host_ip, port)
+
+socket.bind(socket_address)
+socket.listen(5)
+client_socket, addr = socket.accept()
+
+if client_socket:
+    while True:
+        pickMsg = client_socket.recv(64)
+        msg = pickle.loads(pickMsg)
+        print(msg)
